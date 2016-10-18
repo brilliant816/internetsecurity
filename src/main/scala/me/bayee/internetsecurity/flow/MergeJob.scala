8bf8c22ea8e55f2eec65d4cf303af0f6778bcccb
mofan <mofan398419242@qq.com>
2016-10-18 20:11:52 +0800

    add

--- src/main/scala/me/bayee/internetsecurity/flow/MergeJob.scala
+++ src/main/scala/me/bayee/internetsecurity/flow/MergeJob.scala
@@ -14,11 +14,11 @@ import scala.xml.XML
 object MergeJob extends App {
   override def main(args: Array[String]): Unit = {
     val xml = XML.load(this.getClass.getClassLoader.getResourceAsStream("merge_job.xml"))
-    val conf = new SparkConf()
+    val conf = new SparkConf().setAppName("test").setMaster("local[8]")
       .set("spark.hadoop.validateOutputSpecs", "false")
     val sc = new SparkContext(conf)
 
-    (xml \ "inputs").foldLeft[(RDD[(String, (ModelInput, List[String]))])](null) { (rdd, node) =>
+    (xml \ "inputs" \ "input").foldLeft[(RDD[(String, (ModelInput, List[String]))])](null) { (rdd, node) =>
       val pipe = sc.sequenceFile[String, String]((node \ "hdfsPath").text).map(kv => (kv._1, ModelInput.fromModelInput(kv._2)))
       if (rdd == null) pipe.map(kv => (kv._1, (kv._2._1, List(kv._2._2))))
       else rdd.fullOuterJoin(pipe).map {
@@ -37,24 +37,21 @@ object MergeJob extends App {
         iter.foreach {
           case (key, (mi, ids)) =>
             val put = new Put(s"${mi.visitTime.get}#${mi.serverIp.getOrElse("")}".getBytes)
-            put.addColumn("cf".getBytes, "serverIp".getBytes, s"${mi.serverIp.getOrElse("")}".getBytes)
+            put.addColumn("cf".getBytes, "visitTime".getBytes, mi.visitTime.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "clientIp".getBytes, mi.clientIp.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "serverIp".getBytes, mi.serverIp.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "url".getBytes, mi.url.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "httpCode".getBytes, mi.httpCode.getOrElse(-1).toString.getBytes)
+            put.addColumn("cf".getBytes, "referInfo".getBytes, mi.referInfo.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "httpMethod".getBytes, mi.httpMethod.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "userAgent".getBytes, mi.userAgent.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "cookie".getBytes, mi.cookie.getOrElse("").getBytes)
+            put.addColumn("cf".getBytes, "clientRequestSize".getBytes, mi.clientRequestSize.getOrElse(-1).toString.getBytes)
+            put.addColumn("cf".getBytes, "serverResponseSize".getBytes, mi.serverResponseSize.getOrElse(-1).toString.getBytes)
+            put.addColumn("cf".getBytes, "ids".getBytes, ids.mkString(",").getBytes)
             table.put(put)
         }
         table.close
       }
   }
 }
-
-//object Test extends App {
-//  override def main(args: Array[String]): Unit = {
-//    val conf = HBaseConfiguration.create()
-//    conf.set("hbase.zookeeper.property.clientPort", "2181")
-//    conf.set("hbase.zookeeper.quorum", "192.168.0.229")
-//    conf.set("hbase.master","192.168.0.229:600000")
-//
-//    val table = new HTable(conf,"internet_security")
-//    val get = new Get("haha".getBytes())
-//    val result = table.get(get)
-//    println(new String(result.value()))
-//  }
-//}
